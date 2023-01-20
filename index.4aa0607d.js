a128bccf8e9259602d5fe677e7e358cc9913a380
LehezaDmytro <116101169+LehezaDmytro@users.noreply.github.com>
2023-01-20 16:13:11 +0000

    Deploying to gh-pages from @ LehezaDmytro/goit-js-hw-10@2d611ca9708faac7b93fca2b6bbb7da79b806da0 🚀

---
+++ index.4aa0607d.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("input");console.log(o)}();
+//# sourceMappingURL=index.4aa0607d.js.map
---
+++ index.7ea9ec58.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("input");console.log(o);
+//# sourceMappingURL=index.7ea9ec58.js.map
